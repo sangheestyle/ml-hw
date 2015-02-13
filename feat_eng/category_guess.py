fcff74c2ee9920cd2de02aaf061bde6575c5657e
Sanghee Kim <sangheestyle@gmail.com>
2015-02-13 10:39:41 -0700

    hw2fe: 0.81853

--- feat_eng/category_guess.py
+++ feat_eng/category_guess.py
@@ -21,15 +21,14 @@ class LemmaTokenizer(object):
     def __init__(self):
         self.wnl = WordNetLemmatizer()
     def __call__(self, doc):
-        return [self.wnl.lemmatize(t) for t in word_tokenize(doc) if not t.isdigit()]
-
+	return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
 
 class Featurizer:
     def __init__(self):
 	self.vectorizer = CountVectorizer(analyzer = 'word',
                                       stop_words = 'english',
                                       strip_accents = 'ascii',
-                                      preprocessor = remove_spec,
+                                      preprocessor = remove_chars,
                                       tokenizer = LemmaTokenizer(),
                                       ngram_range = (1,2))
 
@@ -45,12 +44,13 @@ class Featurizer:
             top10 = np.argsort(classifier.coef_[i])[-10:]
             print("%s: %s" % (category, " ".join(feature_names[top10])))
 
-def remove_spec(str):
+def remove_chars(text):
     punctuation = re.compile(r'[,.?!|0-9]')
-    str = punctuation.sub('', str)
+    text = punctuation.sub('', text)
 
-    return str
-	
+    return text
+
+
 if __name__ == "__main__":
 
     # Cast to list to keep it all in memory
@@ -64,26 +64,16 @@ if __name__ == "__main__":
         if not line['cat'] in labels:
             labels.append(line['cat'])
 
-    random.shuffle(train)
-    l = len(train)
-
-    x_train = feat.train_feature(x['text'] for x in train[:-5000])
-
-    x_test_in = feat.test_feature(x['text'] for x in train[-5000:l])
+    x_train = feat.train_feature(x['text'] for x in train)
     x_test = feat.test_feature(x['text'] for x in test)
 
-    y_train = array(list(labels.index(x['cat']) for x in train[:-5000]))
-    y_test_in = array(list(labels.index(x['cat']) for x in train[-5000:l]))
+    y_train = array(list(labels.index(x['cat']) for x in train))
 
     # Train classifier
     lr = SGDClassifier(loss='log', penalty='l2', shuffle=True)
     lr.fit(x_train, y_train)
 
     feat.show_top10(lr, labels)
-
-    predictions_in = lr.predict(x_test_in)
-
-    print("Accuracy: %f" % accuracy_score(y_test_in, predictions_in))
 
     predictions = lr.predict(x_test)
     o = DictWriter(open("predictions.csv", 'w'), ["id", "cat"])
